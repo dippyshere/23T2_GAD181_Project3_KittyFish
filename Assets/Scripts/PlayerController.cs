a0531bcf4e67fb01f307e4720fdfad9cd8274a09
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2023-07-28 01:42:02 +1000

    basic character controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private Animator animator;
 
-    [SerializeField] private Animator catAnimator;
+    private bool isWalking;
+    private CharacterController controller;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Temp
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        isWalking = (horizontalInput != 0f || verticalInput != 0f);
+        animator.SetBool("isWalking", isWalking);
+        animator.SetFloat("WalkSpeedMultiplier", Mathf.Clamp(Mathf.Sqrt(horizontalInput * horizontalInput + verticalInput * verticalInput) * walkSpeed * 1.3f, 0, walkSpeed * 1.3f));
+        if (isWalking)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(new Vector3(horizontalInput, 0f, verticalInput));
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * walkSpeed;
+
+        controller.SimpleMove(Vector3.ClampMagnitude(movement, walkSpeed));
     }
 }

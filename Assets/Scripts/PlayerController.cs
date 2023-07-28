1f600bb1875ae90c8bde10f7d9140f3c174c3559
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2023-07-29 04:31:00 +1000

    import assets, add coyote time

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float walkSpeed = 5f;
     [SerializeField] private float gravity = -9.81f;
     [SerializeField] private float jumpHeight = 1f;
+    [SerializeField] private float coyoteTime = 0.1f;
     [SerializeField] private string controlScheme = "KeyboardLeft";
 
     private bool isWalking;
     private bool isGrounded;
+    private bool jumped = false;
+    private float lastTimeGrounded = 0f;
     private Vector3 velocity;
     private Vector2 movementInput = Vector2.zero;
-    private bool jumped = false;
     private PlayerInput playerInput => GetComponent<PlayerInput>();
     private CharacterController controller => GetComponent<CharacterController>();
     private Animator animator => GetComponentInChildren<Animator>();
@@ -44,11 +46,17 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        // Temp
         float horizontalInput = movementInput.x;
         float verticalInput = movementInput.y;
 
         isGrounded = controller.isGrounded;
+        if (isGrounded)
+        {
+            lastTimeGrounded = Time.time;
+        }
+
+        bool canJump = Time.time - lastTimeGrounded <= coyoteTime;
+
         if (isGrounded && velocity.y < 0f)
         {
             velocity.y = 0f;
@@ -57,19 +65,20 @@ public class PlayerController : MonoBehaviour
         isWalking = (horizontalInput != 0f || verticalInput != 0f);
         animator.SetBool("isWalking", isWalking);
         animator.SetFloat("WalkSpeedMultiplier", Mathf.Clamp(Mathf.Sqrt(horizontalInput * horizontalInput + verticalInput * verticalInput) * walkSpeed * 1.3f, 0, walkSpeed * 1.3f));
-        
+
         if (isWalking)
         {
             Quaternion targetRotation = Quaternion.LookRotation(new Vector3(horizontalInput, 0f, verticalInput));
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 14f);
         }
-        
+
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * walkSpeed * Time.deltaTime;
         controller.Move(Vector3.ClampMagnitude(movement, walkSpeed * Time.deltaTime));
 
-        if (jumped && isGrounded)
+        if (jumped && canJump)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -4f * gravity);
+            jumped = false;
         }
 
         velocity.y += gravity * Time.deltaTime;

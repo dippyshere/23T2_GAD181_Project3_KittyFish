31569aa1e62175fdcc8ff12458382acbf087df13
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2023-07-30 22:51:47 +1000

    add basic level

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -16,52 +16,72 @@ public class CameraController : MonoBehaviour
 
     private float initialCameraX;
 
+    public bool trackCats = true;
+    public Transform positionOverride;
+
     private void Start()
     {
-        initialCameraX = Camera.main.transform.position.x;
-        // Modified from ChatGPT
-        Vector3 midpoint = (player1Transform.position + player2Transform.position) * 0.5f;
-        float distanceFromMidpoint = Mathf.Abs(midpoint.x - initialCameraX);
-        float distance = Vector3.Distance(player1Transform.position, player2Transform.position) + distanceFromMidpoint * 1.1f;
-        float targetSize = distance * 1.1f;
-        targetSize = Mathf.Clamp(targetSize, minZoomOutDistance, maxZoomOutDistance);
-        Vector3 desiredCameraPosition = midpoint;
-        desiredCameraPosition -= Camera.main.transform.forward * (targetSize * 0.7f);
-        desiredCameraPosition.y += targetSize * 0.35f;
-        desiredCameraPosition.z -= targetSize * 0.25f;
-        desiredCameraPosition.x = Mathf.Clamp(desiredCameraPosition.x, railStartPoint.x, railEndPoint.x);
-        desiredCameraPosition.z = Mathf.Clamp(desiredCameraPosition.z, railStartPoint.z, railEndPoint.z);
-        Camera.main.transform.position = desiredCameraPosition;
+        if (trackCats)
+        {
+            initialCameraX = Camera.main.transform.position.x;
+            // Modified from ChatGPT
+            Vector3 midpoint = (player1Transform.position + player2Transform.position) * 0.5f;
+            float distanceFromMidpoint = Mathf.Abs(midpoint.x - initialCameraX);
+            float distance = Vector3.Distance(player1Transform.position, player2Transform.position) + distanceFromMidpoint * 1.1f;
+            float targetSize = distance * 1.1f;
+            targetSize = Mathf.Clamp(targetSize, minZoomOutDistance, maxZoomOutDistance);
+            Vector3 desiredCameraPosition = midpoint;
+            desiredCameraPosition -= Camera.main.transform.forward * (targetSize * 0.7f);
+            desiredCameraPosition.y += targetSize * 0.35f;
+            desiredCameraPosition.z -= targetSize * 0.25f;
+            desiredCameraPosition.x = Mathf.Clamp(desiredCameraPosition.x, railStartPoint.x, railEndPoint.x);
+            desiredCameraPosition.z = Mathf.Clamp(desiredCameraPosition.z, railStartPoint.z, railEndPoint.z);
+            Camera.main.transform.position = desiredCameraPosition;
+        }
+        else
+        {
+            Camera.main.transform.position = positionOverride.position;
+            Camera.main.transform.rotation = positionOverride.rotation;
+        }
     }
 
     private void Update()
     {
-        // Calculate the midpoint between the two players
-        Vector3 midpoint = (player1Transform.position + player2Transform.position) * 0.5f;
+        if (trackCats)
+        {
+            // Calculate the midpoint between the two players
+            Vector3 midpoint = (player1Transform.position + player2Transform.position) * 0.5f;
 
-        float distanceFromMidpoint = Mathf.Abs(midpoint.x - initialCameraX);
+            float distanceFromMidpoint = Mathf.Abs(midpoint.x - initialCameraX);
 
-        // Calculate the distance between the two players
-        float distance = Vector3.Distance(player1Transform.position, player2Transform.position) + distanceFromMidpoint * 1.1f;
+            // Calculate the distance between the two players
+            float distance = Vector3.Distance(player1Transform.position, player2Transform.position) + distanceFromMidpoint * 1.1f;
 
-        // Calculate the desired zoom level based on the distance between players
-        float targetSize = distance * 1.1f;
+            // Calculate the desired zoom level based on the distance between players
+            float targetSize = distance * 1.1f;
 
-        // Clamp the desired orthographic size to a maximum value
-        targetSize = Mathf.Clamp(targetSize, minZoomOutDistance, maxZoomOutDistance);
+            // Clamp the desired orthographic size to a maximum value
+            targetSize = Mathf.Clamp(targetSize, minZoomOutDistance, maxZoomOutDistance);
 
-        // Calculate the desired camera position based on the midpoint and the rail limits
-        Vector3 desiredCameraPosition = midpoint;
-        desiredCameraPosition -= Camera.main.transform.forward * (targetSize * 0.7f); // Adjust the multiplier as needed to set the camera distance behind the midpoint
-        desiredCameraPosition.y += targetSize * 0.35f;
-        desiredCameraPosition.z -= targetSize * 0.25f;
+            // Calculate the desired camera position based on the midpoint and the rail limits
+            Vector3 desiredCameraPosition = midpoint;
+            desiredCameraPosition -= Camera.main.transform.forward * (targetSize * 0.7f); // Adjust the multiplier as needed to set the camera distance behind the midpoint
+            desiredCameraPosition.y += targetSize * 0.35f;
+            desiredCameraPosition.z -= targetSize * 0.25f;
 
-        // Limit the camera movement along the rail points
-        desiredCameraPosition.x = Mathf.Clamp(desiredCameraPosition.x, railStartPoint.x, railEndPoint.x);
-        desiredCameraPosition.z = Mathf.Clamp(desiredCameraPosition.z, railStartPoint.z, railEndPoint.z);
+            // Limit the camera movement along the rail points
+            desiredCameraPosition.x = Mathf.Clamp(desiredCameraPosition.x, railStartPoint.x, railEndPoint.x);
+            desiredCameraPosition.z = Mathf.Clamp(desiredCameraPosition.z, railStartPoint.z, railEndPoint.z);
 
-        // Smoothly move the camera to the desired position
-        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, desiredCameraPosition, Time.deltaTime * followSpeed);
+            // Smoothly move the camera to the desired position
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, desiredCameraPosition, Time.deltaTime * followSpeed);
+            Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, Quaternion.Euler(26, 0, 0), Time.deltaTime * followSpeed);
+        }
+        else
+        {
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, positionOverride.position, Time.deltaTime * followSpeed);
+            Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, positionOverride.rotation, Time.deltaTime * followSpeed);
+        }
     }
 
     private void OnDrawGizmos()
